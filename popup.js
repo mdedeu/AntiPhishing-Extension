33f5bf07735f58951d991493eaeb9c3e48f936c4
mdedeu <mdedeu@itba.edu.ar>
2022-02-17 16:52:50 -0300

    api fetch

--- popup.js
+++ popup.js
@@ -1,16 +1,21 @@
-// Initialize button with user's preferred color
 let button = document.getElementById("getButton");
 let probability_number = 40;
-// When the button is clicked, inject setPageBackgroundColor into current page
+
+
 button.addEventListener("click", async () => {
    await getProbability();
   });
 
   async function getProbability() {
+    //const response = await fetch('http://example.com/movies.json');
+    //const myJson = await response.json();
+
+    //probability_number = myJson.probability;
 
     let probability=  document.getElementById("probability")
 
-    probability.style =" display: flex"
+    probability.style =" display: flex" 
+
 
     probability.textContent = probability_number+ "%"
     if(probability_number < 20){
